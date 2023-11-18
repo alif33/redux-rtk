4ecb707baf8468df5f1df26c8208a3b82f7882e7
alif33 <alifhasan332@gmail.com>
2023-11-18 18:59:34 +0600

    apply

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,10 @@ import './App.css'
 import { useGetAllProductsQuery } from './features/apiSlice'
 
 function App() {
-  const { data, error, isLoading } = useGetAllProductsQuery();
   // called the query hook
   return (
     <div>
-      {
-        isLoading?  <h1>loading...</h1>:  <table>
-        <thead>
-          <tr style={{ fontSize: 20 }}>
-            <td>Product name</td>
-            <td>Image</td>
-            <td>Brand</td>
-            <td>Price</td>
-            <td>Rating</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.products?.map((item, index)=><tr key={index}>
-            <td>{item.title}</td>
-            <td><img
-              height={100}
-              width={100}
-              src={item.images[0]}
-              /></td>
-            <td>{item.brand}</td>
-            <td>{item.price}</td>
-            <td>{item.rating}</td>
-          </tr>)}
-         
-        </tbody>
-      </table>
-      }
-     
+     <h1>This test branch.</h1>
     </div>
   )
 }
